51f3fb33691bfb700566766815f2e0eb37183d61
Jeff Talbot <jeff.a.talbot@gmail.com>
2014-01-31 07:36:48 -0600

    Initial rails app generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Swipewords::Application.config.secret_key_base = '638a20e498825c7d70151271642fe27090092a4af4cf592b4711e2179c11b692ffca4e7dae68b8d4acdcd1d6f00696737133707671aa370f0a3fcc0f0510e623'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Swipewords::Application.config.session_store :cookie_store, key: '_swipewords_session'
