1eb4168594479b1bb4d782ca15f87df2991b0855
Jeff Talbot <jeff.a.talbot@gmail.com>
2014-02-07 07:40:00 -0600

    Added a few more words to the library

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,25 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-image_path = "#{Rails.root}/spec/fixtures/"
+def image_path
+  "#{Rails.root}/spec/fixtures/"
+end
 
-Word.create!(
-  :value => 'brick',
-  :prefix => 'br',
-  :suffix => 'ick',
-  :picture => File.new(image_path + "brick.jpg")
-)
+def create_word(value, prefix, suffix, filename)
+
+  Word.create!(
+    :value => value,
+    :prefix => prefix,
+    :suffix => suffix,
+    :picture => File.new(image_path + filename)
+  )
+end
+
+create_word "brick", "br", "ick", "brick.jpg"
+
+create_word "shirt", "sh", "irt", "shirt.jpg"
+
+create_word "plant", "pl", "ant", "plant.jpg"
+
+create_word "tree", "tr", "ee", "tree.jpg"
 
