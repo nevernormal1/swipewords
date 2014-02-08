8d7efe00c07dab443f806e0eb2203b04dbba5707
Jeff Talbot <jeff.a.talbot@gmail.com>
2014-02-08 07:59:39 -0600

    Changed prefix/suffix methods

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,12 +1,20 @@
 class Word < ActiveRecord::Base
-  has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :picture,
+    :styles => {
+      :medium => "300x300>",
+      :thumb => "100x100>"
+    },
+    :default_url => "/images/:style/missing.png"
+
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 
+  scope :random, limit(4).order("RANDOM()")
+
   def prefixes
-    %w(spr fr br cl)
+    ((self.class.random.pluck(:prefix) - [prefix])[0..-1] + [prefix]).shuffle
   end
 
   def suffixes
-    %w(ight ick oud ate)
+    ((self.class.random.pluck(:suffix) - [suffix])[0..-1] + [suffix]).shuffle
   end
 end
