99d674fd9a16dd2630294bf5fb88fc1444defb19
Jeff Talbot <jeff.a.talbot@gmail.com>
2014-02-12 07:26:47 -0600

    Added 8 more words

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,9 +23,34 @@ end
 
 create_word "brick", "br", "ick", "brick.jpg"
 
-create_word "shirt", "sh", "irt", "shirt.jpg"
+create_word "clam", "cl", "am", "clam.jpg"
+create_word "cloud", "cl", "oud", "cloud.jpg"
+create_word "cow", "c", "cow", "cow.jpg"
+
+create_word "flower", "fl", "ower", "flower.jpg"
 
 create_word "plant", "pl", "ant", "plant.jpg"
+create_word "plow", "pl", "ow", "plow.jpg"
+
+create_word "shark", "sh", "ark", "shark.jpg"
+create_word "shirt", "sh", "irt", "shirt.jpg"
+create_word "skunk", "sk", "unk", "skunk.jpg"
+create_word "snail", "sn", "ail", "snail.jpg"
 
 create_word "tree", "tr", "ee", "tree.jpg"
 
+
+
+
+# Ar
+# An
+#
+# Br
+# Cr
+# Dr
+#
+# Fl
+# Fr
+# Gr
+# Pr
+#
